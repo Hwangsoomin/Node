cdfae2dcbf492a4dc5c55719f9f39b049e2615fc
Hwangsoomin <maum97@naver.com>
2021-01-04 04:13:10 +0900

    Add Module winston, winston-daily-rotate-file & Update .gitignore

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,12 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import logger from 'morgan';
+import { logger } from './config/winston';
 import dotenv from 'dotenv';
 
 var app = express();
 dotenv.config();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
