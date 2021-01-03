a7067f1febea87b32ccceb19c6c1db86380ee39b
Hwangsoomin <maum97@naver.com>
2021-01-04 04:14:30 +0900

    Connect MongoDB

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import { logger } from './config/winston';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 var app = express();
 dotenv.config();
@@ -14,5 +15,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function () {
+    logger.info('Connected to mongod server');
+});
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
 
 module.exports = app;
