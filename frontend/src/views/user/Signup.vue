1ece55e582732f7076e9778a1f4cb062a77142f8
Hwangsoomin <maum97@naver.com>
2021-01-19 20:16:02 +0900

    Add Signup Page & Update App.vue Page(router-view)

---
+++ frontend/src/views/user/Signup.vue
@@ -0,0 +1,99 @@
+<template>
+	<v-card class="ma-10 pa-10 fill-width" outlined>
+		<v-list-item class="pl-0">
+			<v-list-item-content>
+				<div>
+					SIGN UP
+				</div>
+			</v-list-item-content>
+		</v-list-item>
+		<v-form ref="form" lazy-validation>
+			<v-row>
+				<v-col>
+					<v-text-field
+						v-model="name"
+						:counter="10"
+						:rules="nameRules"
+						label="Name"
+						required
+					></v-text-field>
+				</v-col>
+
+				<v-col>
+					<v-select
+						v-model="gender"
+						:items="items"
+						:rules="[v => !!v || '성별을 선택해주세요!']"
+						label="성별"
+						required
+					></v-select>
+				</v-col>
+			</v-row>
+
+			<v-row class="pa-3">
+				<v-text-field
+					v-model="email"
+					:rules="emailRules"
+					label="E-mail"
+					required
+				></v-text-field>
+			</v-row>
+
+			<v-row class="pa-3">
+				<v-text-field
+					v-model="id"
+					:rules="idRules"
+					label="ID"
+					required
+				></v-text-field>
+			</v-row>
+
+			<v-row class="pa-3">
+				<v-text-field
+					v-model="password"
+					:rules="passwordRules"
+					type="password"
+					label="PASSWORD"
+					required
+				>
+				</v-text-field>
+			</v-row>
+
+			<v-row class="justify-center">
+				<v-btn depressed color="pink lighten-2" style="color: white;">
+					회원가입
+				</v-btn>
+			</v-row>
+		</v-form>
+	</v-card>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		valid: true,
+		name: '',
+		nameRules: [v => !!v || '이름을 입력해 주세요!'],
+		email: '',
+		emailRules: [
+			v => !!v || '이메일을 입력해 주세요!',
+			v => /.+@.+\..+/.test(v) || '유효한 이메일이 아닙니다.',
+		],
+		id: '',
+		idRules: [v => !!v || 'ID를 입력해 주세요!'],
+		gender: null,
+		items: ['남성', '여성'],
+		password: '',
+		passwordRules: [
+			v => !!v || '비밀번호를 입력해 주세요!',
+			v =>
+				/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,}$/.test(
+					v,
+				) ||
+				'비밀번호는 최소 하나의 문자  + 하나의 숫자 + 하나의 특수 문자 포함, 최소 6자리여야 합니다.',
+		],
+	}),
+
+	methods: {},
+};
+</script>
