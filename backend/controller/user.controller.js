26463881ba4be1e228a865dd9ef0676dbec46a2e
Hwangsoomin <maum97@naver.com>
2021-01-24 21:14:27 +0900

    Delete logger.info(user.controller.js)

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,11 +1,9 @@
 import { UserService } from '../service/user.service.js';
 import { Response } from '../model/Response.js';
-import { logger } from '../config/winston';
 
 export class UserController {
   static createUser = async (req, res, next) => {
     try {
-      logger.info(req.body);
       const result = await UserService.createUser(req.body);
       return res
         .status(201)
