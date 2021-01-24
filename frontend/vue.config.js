e28027b078eb04d8a1adb72fca03a044598ab042
Hwangsoomin <maum97@naver.com>
2021-01-24 22:50:21 +0900

    Add proxy to connect api

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
 	transpileDependencies: ['vuetify'],
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000/api',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': '',
+				},
+			},
+		},
+	},
 };
