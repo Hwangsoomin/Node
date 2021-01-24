ac12a9aeb0dd720899560997e088d400e94747c5
Hwangsoomin <maum97@naver.com>
2021-01-24 22:51:50 +0900

    Update Signup Page

--- frontend/src/views/user/Signup.vue
+++ frontend/src/views/user/Signup.vue
@@ -1,74 +1,89 @@
 <template>
-	<v-card class="ma-10 pa-10 fill-width" outlined>
-		<v-list-item class="pl-0">
-			<v-list-item-content>
-				<div>
-					SIGN UP
-				</div>
-			</v-list-item-content>
-		</v-list-item>
-		<v-form ref="form" lazy-validation>
-			<v-row>
-				<v-col>
+	<v-container justify="center">
+		<v-card class="ma-10 pa-10" style="max-width: 500px;" outlined>
+			<v-list-item class="pl-0">
+				<v-list-item-content>
+					<div>
+						SIGN UP
+					</div>
+				</v-list-item-content>
+			</v-list-item>
+			<v-form ref="form" v-model="valid" lazy-validation>
+				<v-row>
+					<v-col>
+						<v-text-field
+							v-model="name"
+							:counter="10"
+							:rules="nameRules"
+							label="Name"
+							required
+						></v-text-field>
+					</v-col>
+
+					<v-col>
+						<v-select
+							v-model="gender"
+							:items="items"
+							:rules="[v => !!v || '성별을 선택해주세요!']"
+							label="성별"
+							required
+						></v-select>
+					</v-col>
+				</v-row>
+
+				<v-row class="pa-3">
 					<v-text-field
-						v-model="name"
-						:counter="10"
-						:rules="nameRules"
-						label="Name"
+						v-model="email"
+						:rules="emailRules"
+						label="E-mail"
 						required
 					></v-text-field>
-				</v-col>
+				</v-row>
 
-				<v-col>
-					<v-select
-						v-model="gender"
-						:items="items"
-						:rules="[v => !!v || '성별을 선택해주세요!']"
-						label="성별"
+				<v-row class="pa-3">
+					<v-text-field
+						v-model="id"
+						:rules="idRules"
+						label="ID"
 						required
-					></v-select>
-				</v-col>
-			</v-row>
+					></v-text-field>
+				</v-row>
 
-			<v-row class="pa-3">
-				<v-text-field
-					v-model="email"
-					:rules="emailRules"
-					label="E-mail"
-					required
-				></v-text-field>
-			</v-row>
+				<v-row class="pa-3">
+					<v-text-field
+						v-model="password"
+						:rules="passwordRules"
+						type="password"
+						label="PASSWORD"
+						required
+					>
+					</v-text-field>
+				</v-row>
 
-			<v-row class="pa-3">
-				<v-text-field
-					v-model="id"
-					:rules="idRules"
-					label="ID"
-					required
-				></v-text-field>
-			</v-row>
-
-			<v-row class="pa-3">
-				<v-text-field
-					v-model="password"
-					:rules="passwordRules"
-					type="password"
-					label="PASSWORD"
-					required
-				>
-				</v-text-field>
-			</v-row>
-
-			<v-row class="justify-center">
-				<v-btn depressed color="pink lighten-2" style="color: white;">
-					회원가입
-				</v-btn>
-			</v-row>
-		</v-form>
-	</v-card>
+				<v-row class="justify-center">
+					<v-btn
+						depressed
+						color="pink lighten-2"
+						style="color: white;"
+						v-on:click="register()"
+					>
+						회원가입
+					</v-btn>
+				</v-row>
+			</v-form>
+		</v-card>
+		<v-snackbar v-model="snackbar.show" :timeout="2000" :color="snackbar.color">
+			{{ snackbar.text }}
+			<v-btn justify="center" text v-on:click="snackbar.show = false">
+				close
+			</v-btn>
+		</v-snackbar>
+	</v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
 	data: () => ({
 		valid: true,
@@ -92,8 +107,36 @@ export default {
 				) ||
 				'비밀번호는 최소 하나의 문자  + 하나의 숫자 + 하나의 특수 문자 포함, 최소 6자리여야 합니다.',
 		],
+		snackbar: {
+			show: false,
+			text: '',
+			color: '',
+		},
 	}),
 
-	methods: {},
+	methods: {
+		async register() {
+			await this.$refs.form.validate();
+			if (!this.valid) return;
+			try {
+				const result = await axios.post('/api/user/signup', {
+					name: this.name,
+					gender: this.gender,
+					email: this.email,
+					id: this.id,
+					password: this.password,
+				});
+				if (result.status === 201) {
+					this.snackbar.color = 'success';
+					this.snackbar.text = '회원가입에 성공하셨습니다!';
+					this.snackbar.show = true;
+				}
+			} catch (err) {
+				this.snackbar.color = 'error';
+				this.snackbar.text = '이미 사용 중인 아이디 입니다.';
+				this.snackbar.show = true;
+			}
+		},
+	},
 };
 </script>
