8aa1cbfea1eb8d9ce1498416d10dd4f1d1e470d2
Hwangsoomin <maum97@naver.com>
2021-01-26 22:29:55 +0900

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import { UserController } from '../controller/user.controller.js';
+import validate from '../validate/user.validate.js';
 
-router.post('/signup', UserController.createUser);
-router.post('/login', UserController.login);
+router.post('/signup', validate.createUser, UserController.createUser);
+router.post('/login', validate.login, UserController.login);
 
 module.exports = router;
