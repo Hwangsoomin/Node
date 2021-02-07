be250ab4b69fa6728c5f6930c365e45761e83a74
Hwangsoomin <maum97@naver.com>
2021-02-07 23:42:15 +0900

    Update index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,31 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {},
-	mutations: {},
-	actions: {},
+	state: { accessToken: null, id: null, name: null },
+	mutations: {
+		Login(state, { accessToken, user }) {
+			state.accessToken = accessToken;
+			state.id = user.id;
+			state.name = user.name;
+		},
+	},
+	actions: {
+		async Login({ commit }, { id, password }) {
+			const result = await axios.post('/api/user/login', {
+				id,
+				password,
+			});
+			axios.defaults.headers.common = {
+				Authorization: 'Bearer ' + result.data.result.accessToken,
+			};
+			return commit('Login', result.data.result);
+		},
+	},
 	modules: {},
+	plugins: [createPersistedState()],
 });
