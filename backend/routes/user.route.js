271dd9d5b7a25c02acec30d46b2756e91aff76e2
Hwangsoomin <maum97@naver.com>
2021-02-08 00:36:56 +0900

    Update user.route.js(add validate, authmiddleware, API)

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { UserController } from '../controller/user.controller.js';
-import validate from '../validate/user.validate.js';
+import { UserController } from '../controller/user.controller';
+import validate from '../validate/user.validate';
+import authMiddleware from '../middleware/auth.middleware';
 
 router.post('/signup', validate.createUser, UserController.createUser);
 router.post('/login', validate.login, UserController.login);
+router.get('/profile/:id', authMiddleware(), UserController.getUser);
 
 module.exports = router;
